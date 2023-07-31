506501ce5ff6578febd5ca0f4fdba04791d3a298
suria-raul <stadnitzki.nero.nero@gmail.com>
2023-07-31 14:16:34 +0800

    answer to php task

---
+++ index.php
@@ -0,0 +1,78 @@
+<?php
+
+// I assume that you will be using the php cli to run this
+
+
+require 'vendor/autoload.php';
+
+use jc21\CliTable;
+
+function displayTable($transactions, $users)
+{
+    $newArr = array_map(function($transaction, $user) {
+        return [
+            "id" => $user['id'],
+            "type" => $transaction['type'],
+            "name" => $user['name']
+        ];
+    }, $transactions, $users);
+
+    // $newArr = [];
+    // for($i = 0; $i < count($users); $i++) {
+    //     $newArr[] = [
+    //         "id" => $i,
+    //         "type" => $transactions[$i]['type'],
+    //         "name" => $users[$i]['name']
+    //     ];
+    // }
+
+    $table = new CliTable;
+    $table->setTableColor('blue');
+    $table->setHeaderColor('cyan');
+    $table->addField('id', 'id', false, 'white');
+    $table->addField('type', 'type', false, 'white');
+    $table->addField('name', 'name', false, 'white');
+    $table->injectData($newArr);
+    $table->display();
+}
+
+$transactions = [
+    [
+        "id" => 1,
+        "type" => "cash",
+        "agent_id" => 2,
+        "created_at" => "2020-01-31 08:22:53"
+    ],
+    [
+        "id" => 2,
+        "type" => "credit",
+        "agent_id" => 3,
+        "created_at" => "2020-04-01 09:43:22"
+    ],
+    [
+        "id" => 3,
+        "type" => "cash",
+        "agent_id" => 1,
+        "created_at" => "2020-06-04 03:56:12"
+    ]
+];
+
+$users = [
+    [
+        "id" => 1,
+        "name" => "Mike Ross",
+        "updated_at" => "2020-01-30 12:11:43"
+    ],
+    [
+        "id" => 2,
+        "name" => "Tedd Mosby",
+        "updated_at" => "2020-02-06 09:27:13"
+    ],
+    [
+        "id" => 3,
+        "name" => "Peter Parker",
+        "updated_at" => "2020-06-04 03:56:12"
+    ]
+];
+
+displayTable($transactions, $users);
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'jc21/clitable' => array(
+            'pretty_version' => '1.2',
+            'version' => '1.2.0.0',
+            'reference' => 'a9ff1fcdff131d25e90a9ae839a5805f6963b3da',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../jc21/clitable',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
